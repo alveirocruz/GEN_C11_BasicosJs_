ebc8a6a2e0b8c13070c51e1a63d09001fd29bc46
 <anaa_alvarad@hotmail.com>
2026-03-24 12:09:12 -0500

    Modulos

--- modulos/index.js
+++ modulos/index.js
@@ -1,33 +1,15 @@
-class Usuario {
-  constructor(nombre, email) {
-    this.nombre = nombre;
-    this.email  = email;
-  }
-  saludo() {
-    return "Hola, soy " + this.nombre;
-  }
-}
+//! Forma de hacerlo en CommonJs 
+//const Usuario = require("./usuario")
+import Usuario from "./usuario.js";
+import Producto from "./producto.js";
+import { formatearPrecio,validarEmail, calcularDescuento } from "./funciones.js";
 
-class Producto {
-  constructor(nombre, precio) {
-    this.nombre = nombre;
-    this.precio = precio;
-  }
-  calcularTotal() {
-    return this.precio * 1.19;
-  }
-}
 
-function formatearPrecio(precio) {
-  return "$" + precio.toLocaleString("es-CO");
-}
+let u1 = new Usuario("Ana", "ana@email.com");
+let producto1  = new Producto("Teclado", 250000);
 
-function validarEmail(email) {
-  return email.includes("@") && email.includes(".");
-}
-
-let u1      = new Usuario("Ana", "ana@email.com");
-let p1      = new Producto("Teclado", 250000);
+let u = new Usuario("Ana", "ana@email.com");
 
 console.log(u1.saludo());
-console.log(formatearPrecio(p1.calcularTotal()));
+console.log(formatearPrecio(producto1.calcularTotal()));
+console.log(u);
